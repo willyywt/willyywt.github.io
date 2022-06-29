a6e436b9bfa85f62ef3af984cb0e2bb57b772191
willyang <22425315+willyywt@users.noreply.github.com>
2022-06-29 20:39:11 +0800

    margaritic-spurriers-autocombustible-overabstain-all-efficient-horse-godmothercaciqueCovenanttungusicageless

--- assets/js/pref.js
+++ assets/js/pref.js
@@ -6,12 +6,12 @@ function input_changed_cb(ev) {
   Json2Cookie()
   CookieLevelHook(name, val)
 }
-for (var el_index = 0; el_index < pref_input_el_arr.length; el_index++) {
+for (var el_index = 0; el_index < pref_input_el_arr.length; el_index += 1) {
   var el = pref_input_el_arr[el_index]
   el.addEventListener('change', input_changed_cb)
 }
 function PrefLegendCheckSync() {
-  for (var name_index = 0; name_index < LegendName_arr.length; name_index++) {
+  for (var name_index = 0; name_index < LegendName_arr.length; name_index += 1) {
     var name = LegendName_arr[name_index]
     var value = cookie_json[name]
     console.log(name, value)
