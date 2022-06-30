df590044c0abb72b97e6ebd51c94de373465b98e
willyang <22425315+willyywt@users.noreply.github.com>
2022-06-30 19:18:23 +0800

    [HandWritten] Delete default value cookie by making it expire immediately

--- assets/js/pref.js
+++ assets/js/pref.js
@@ -1,9 +1,16 @@
 (function(){
 function Json2Cookie_one(key, value_unenc) {
-	var value = encodeURIComponent(value_unenc)
-	var cookie_str_1 = key.concat("=", value, "; ")
-	cookie_str_1 += "Path=/; "
-	document.cookie = cookie_str_1 + "SameSite=None; Max-Age=851472000; Secure"
+	var cookie_str_1 = ""
+	if (value_unenc) {
+		var value = encodeURIComponent(value_unenc)
+		cookie_str_1 = key.concat("=", value, "; ")
+		cookie_str_1 += "Path=/; SameSite=None; Max-Age=851472000; Secure"
+	} else {
+		/* Makes cookie expire immediately */
+		var value = ""
+		cookie_str_1 = key + "=; Path=/; SameSite=None; Max-Age=0; Secure"
+	}
+	document.cookie = cookie_str_1
 }
 function CookieChangeHook(name, val) {
 	var LegendDefault_map = {
@@ -31,9 +38,7 @@ for (var el_index = 0; el_index < pref_input_el_arr.length; el_index += 1) {
 }
 function Json2Cookie() {
 	for (key in cookie_json) {
-		if (cookie_json[key]) {
-			Json2Cookie_one(key, cookie_json[key])
-		}
+		Json2Cookie_one(key, cookie_json[key])
 	}
 }
 /* PrefLegendCheckSync() */
