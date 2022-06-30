b6e33904577ee91043f752a517ea50b077ede22e
willyang <22425315+willyywt@users.noreply.github.com>
2022-06-30 15:18:50 +0800

    [HandWritten] Reorganize javascript .js files to make them compress better

--- _includes/load.js
+++ _includes/load.js
@@ -1,147 +1,149 @@
 var cookie_json = {}
 var constvals = {
-  "cssdark": '/assets/css/main-dark.css',
-  "csslight": '/assets/css/main.css',
-  "cssunhide": '/assets/css/unhide.css'
+	"cssdark": '/assets/css/main-dark.css',
+	"csslight": '/assets/css/main.css',
+	"cssunhide": '/assets/css/unhide.css'
 }
 var cssId = 'maincss';
 var csshookId = 'hookcss';
-function CookieParsePair(start) {
-  var c_start = start
-  var c_end = start
-  var result = {"index": start, "key": "", "value": ""}
-  if (document.cookie.length == 0) {
-    return result
-  }
-  c_start = document.cookie.indexOf("=", start)
-  if (c_start == -1) {
-  	return result
-  }
-  c_end = document.cookie.indexOf(";", c_start + 1)
-  if (c_end == -1) {
-  	c_end = document.cookie.length
-  }
-  result.index = c_end + 2; // Jump "; "
-  result.key = document.cookie.substring(start, c_start)
-  result.value = document.cookie.substring(c_start + 1, c_end)
-  return result
-}
 function Cookie2Json() {
-  var index_start = 0;
-  while (index_start < document.cookie.length) {
-    var parse_result = CookieParsePair(index_start)
-    if (parse_result.key.length == 0 || parse_result.index <= index_start) {
-      return
-    }
-    index_start = parse_result.index
-    var key = parse_result.key
-    var val = parse_result.value
-    cookie_json[key] = decodeURIComponent(val)
-  }
-}
-function StyleInsertLink(conf) {
-   var head  = document.getElementsByTagName('head')[0]
-   var link  = document.createElement('link')
-   link.id   = conf.id
-   link.rel  = conf.rel || 'stylesheet'
-   link.type = conf.type || 'text/css'
-   if(conf.href) link.href = conf.href
-   link.textContent = conf.textContent || ''
-   link.media = conf.media || 'all'
-   head.appendChild(link);
-}
-function StyleInsertStyle(conf) {
-   var head  = document.getElementsByTagName('head')[0]
-   var style = document.createElement('style')
-   style.id = conf.id
-   style.textContent = conf.textContent || ''
-   head.appendChild(style);
+	function CookieParsePair(start) {
+		var c_start = start
+		var c_end = start
+		var result = {"index": start, "key": "", "value": ""}
+		if (document.cookie.length == 0) {
+			return result
+		}
+		c_start = document.cookie.indexOf("=", start)
+		if (c_start == -1) {
+			return result
+		}
+		c_end = document.cookie.indexOf(";", c_start + 1)
+		if (c_end == -1) {
+			c_end = document.cookie.length
+		}
+		result.index = c_end + 2; // Jump "; "
+		result.key = document.cookie.substring(start, c_start)
+		result.value = document.cookie.substring(c_start + 1, c_end)
+		return result
+	}
+	var index_start = 0;
+	while (index_start < document.cookie.length) {
+		var parse_result = CookieParsePair(index_start)
+		if (parse_result.key.length == 0 || parse_result.index <= index_start) {
+			return
+		}
+		index_start = parse_result.index
+		var key = parse_result.key
+		var val = parse_result.value
+		cookie_json[key] = decodeURIComponent(val)
+	}
 }
 function CookieLevelHook(name, value) {
-  var CookieLevelHookCb_dict = {
-    "name-pref-font": PrefFontCb,
-    "name-pref-theme": PrefThemeCb,
-  }
-  var cbfunc = CookieLevelHookCb_dict[name]
-  cbfunc && cbfunc(value)
+	function PrefFontCb(value) {
+		var FontFamily_dict = {
+			"pref-font-default": {"lineHeight": "", "fontFamily": ""},
+			"pref-font-brand": {"lineHeight": "1.5", "fontFamily": "-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,Oxygen-Sans,Ubuntu,Cantarell,\"Helvetica Neue\",sans-serif"},
+			"pref-font-system-ui": {"lineHeight": "1.5", "fontFamily": "system-ui, sans"}
+		}
+		var csshook_el = document.getElementById(csshookId)
+		if (value == "pref-font-default") {
+			csshook_el.textContent = ""
+			return
+		}
+		var css_str = "html body{"
+		if (value in FontFamily_dict) {
+			var lh = FontFamily_dict[value].lineHeight
+			if (lh) {
+				css_str += "line-height: "
+				css_str += lh
+				css_str += ";"
+			}
+			var ff = FontFamily_dict[value].fontFamily
+			if (ff) {
+				css_str += "font-family: "
+				css_str += ff
+				css_str += ";"
+			}
+			css_str += "}"
+		}
+		csshook_el.textContent = css_str
+	}
+	function PrefThemeCb(value) {
+		var main_el = document.getElementById(cssId)
+		var show_el = document.getElementById(cssId + 'show')
+		if (!main_el || !show_el) {
+			return
+		}
+		var is_dark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+		show_el.href = ""
+		var target = ""
+		if (value === "pref-theme-default") {
+			target = is_dark ? constvals.cssdark : constvals.csslight
+		} else if (value === "pref-theme-dark") {
+			target = constvals.cssdark
+		} else if (value == "pref-theme-light") {
+			target = constvals.csslight
+		}
+		if (main_el.href != target) {
+			main_el.href = target;
+		}
+		show_el.href = constvals.cssunhide
+	}
+	var CookieLevelHookCb_dict = {
+		"name-pref-font": PrefFontCb,
+		"name-pref-theme": PrefThemeCb,
+	}
+	var cbfunc = CookieLevelHookCb_dict[name]
+	cbfunc && cbfunc(value)
 }
+(function(){
 function CookieLevelHook_doall() {
-  for (name in cookie_json) {
-    var value = cookie_json[name]
-    if (value) {
-      CookieLevelHook(name, value)
-    }
-  }
+	for (name in cookie_json) {
+		var value = cookie_json[name]
+		if (value) {
+			CookieLevelHook(name, value)
+		}
+	}
 }
-function PrefFontCb(value) {
-  var FontFamily_dict = {
-    "pref-font-default": {"lineHeight": "", "fontFamily": ""},
-    "pref-font-brand": {"lineHeight": "1.5", "fontFamily": "-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,Oxygen-Sans,Ubuntu,Cantarell,\"Helvetica Neue\",sans-serif"},
-    "pref-font-system-ui": {"lineHeight": "1.5", "fontFamily": "system-ui, sans"}
-  }
-  var csshook_el = document.getElementById(csshookId)
-  if (value == "pref-font-default") {
-     csshook_el.textContent = ""
-     return
-  }
-  var css_str = "html body{"
-  if (value in FontFamily_dict) {
-    var lh = FontFamily_dict[value].lineHeight
-    if (lh) {
-      css_str += "line-height: "
-      css_str += lh
-      css_str += ";"
-    }
-    var ff = FontFamily_dict[value].fontFamily
-    if (ff) {
-      css_str += "font-family: "
-      css_str += ff
-      css_str += ";"
-    }
-    css_str += "}"
-  }
-  csshook_el.textContent = css_str
+function StyleInsertLink(conf) {
+	var head = document.getElementsByTagName('head')[0]
+	var link = document.createElement('link')
+	link.id	= conf.id
+	link.rel = conf.rel || 'stylesheet'
+	link.type = conf.type || 'text/css'
+	if(conf.href) link.href = conf.href
+	link.textContent = conf.textContent || ''
+	link.media = conf.media || 'all'
+	head.appendChild(link);
 }
-function PrefThemeCb(value) {
-  var main_el = document.getElementById(cssId)
-  var show_el = document.getElementById(cssId + 'show')
-  if (!main_el || !show_el) {
-    return
-  }
-  var is_dark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-  show_el.href = ""
-  var target = ""
-  if (value === "pref-theme-default") {
-    target = is_dark ? constvals.cssdark : constvals.csslight
-  } else if (value === "pref-theme-dark") {
-    target = constvals.cssdark
-  } else if (value == "pref-theme-light") {
-    target = constvals.csslight
-  }
-  if (main_el.href != target) {
-    main_el.href = target;
-  }
-  show_el.href = constvals.cssunhide
+function StyleInsertStyle(conf) {
+	var head = document.getElementsByTagName('head')[0]
+	var style = document.createElement('style')
+	style.id = conf.id
+	style.textContent = conf.textContent || ''
+	head.appendChild(style);
 }
 if (!document.getElementById(cssId))
 {
-   var is_dark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
-   StyleInsertLink({
-     "id": cssId,
-     "href": is_dark ? constvals.cssdark : constvals.csslight
-   })
-   StyleInsertLink({
-     "id": cssId + 'show',
-     "href": constvals.cssunhide
-   })
+	var is_dark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+	StyleInsertLink({
+		"id": cssId,
+		"href": is_dark ? constvals.cssdark : constvals.csslight
+	})
+	StyleInsertLink({
+		"id": cssId + 'show',
+		"href": constvals.cssunhide
+	})
 }
 if (!document.getElementById(csshookId))
 {
-   StyleInsertStyle({
-     "id": csshookId
-   })
+	StyleInsertStyle({
+		"id": csshookId
+	})
 }
 if (document.cookie.length > 0) {
 Cookie2Json()
 CookieLevelHook_doall()
 }
+})()
--- assets/js/pref.js
+++ assets/js/pref.js
@@ -1,50 +1,52 @@
-pref_input_el_arr = document.getElementsByTagName("input");
-function input_changed_cb(ev) {
-  var val = ev.target.value
-  var name = ev.target.name
-  CookieChangeHook(name, val)
-}
-for (var el_index = 0; el_index < pref_inoput_el_arr.length; el_index += 1) {
-  var el = pref_input_el_arr[el_index]
-  el.addEventListener('change', input_changed_cb)
-}
-function Json2Cookie() {
-  for (key in cookie_json) {
-    if (cookie_json[key]) {
-      Json2Cookie_one(key, cookie_json[key])
-    }
-  }
-}
+(function(){
 function Json2Cookie_one(key, value_unenc) {
-  var value = encodeURIComponent(value_unenc)
-  var cookie_str_1 = key.concat("=", value, "; ")
-  cookie_str_1 += "Path=/; "
-  document.cookie = cookie_str_1 + "SameSite=None; Max-Age=851472000; Secure"
+	var value = encodeURIComponent(value_unenc)
+	var cookie_str_1 = key.concat("=", value, "; ")
+	cookie_str_1 += "Path=/; "
+	document.cookie = cookie_str_1 + "SameSite=None; Max-Age=851472000; Secure"
 }
 function CookieChangeHook(name, value) {
-var LegendDefault_map = {
-	"name-pref-font": "pref-font-default",
-	"name-pref-theme": "pref-theme-default"
+	var LegendDefault_map = {
+		"name-pref-font": "pref-font-default",
+		"name-pref-theme": "pref-theme-default"
+	}
+	CookieLevelHook(name, val)
+	if(LegendDefault_map[name] === val) {
+		cookie_json[name] = ""
+	} else {
+		cookie_json[name] = val
+	}
+	Json2Cookie()
 }
-  CookieLevelHook(name, val)
-  if(LegendDefault_map[name] === val) {
-	  cookie_json[name] = ""
-  } else {
-	  cookie_json[name] = val
-  }
-  Json2Cookie()
+/* PrefSetCb() */
+pref_input_el_arr = document.getElementsByTagName("input");
+function input_changed_cb(ev) {
+	var val = ev.target.value
+	var name = ev.target.name
+	CookieChangeHook(name, val)
 }
-function PrefLegendCheckSync() {
-  var LegendName_arr = ["name-pref-font", "name-pref-theme"]
-  for (var name_index = 0; name_index < LegendName_arr.length; name_index += 1) {
-    var name = LegendName_arr[name_index]
-    var value = cookie_json[name]
-    console.log(name, value)
-    var input_el = document.getElementById(value)
-    console.log(input_el)
-    if (input_el) {
-      input_el.checked = true
-    }
-  }
+for (var el_index = 0; el_index < pref_inoput_el_arr.length; el_index += 1) {
+	var el = pref_input_el_arr[el_index]
+	el.addEventListener('change', input_changed_cb)
 }
-PrefLegendCheckSync()
+function Json2Cookie() {
+	for (key in cookie_json) {
+		if (cookie_json[key]) {
+			Json2Cookie_one(key, cookie_json[key])
+		}
+	}
+}
+/* PrefLegendCheckSync() */
+var LegendName_arr = ["name-pref-font", "name-pref-theme"]
+for (var name_index = 0; name_index < LegendName_arr.length; name_index += 1) {
+	var name = LegendName_arr[name_index]
+	var value = cookie_json[name]
+	console.log(name, value)
+	var input_el = document.getElementById(value)
+	console.log(input_el)
+	if (input_el) {
+		input_el.checked = true
+	}
+}
+})()
+
