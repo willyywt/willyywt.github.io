648c4f61e3c389d179eca75d2db7a5c6daa45cc9
willyang <22425315+willyywt@users.noreply.github.com>
2022-07-01 21:15:13 +0800

    podagra-chowder-crankcases-orthometopic-easternmost-yeguitacosmographistuncorruptnesssubbassestafia

--- assets/js/pref.js
+++ assets/js/pref.js
@@ -12,11 +12,13 @@ function Json2Cookie_one(key, value_unenc) {
 	}
 	document.cookie = cookie_str_1
 }
+var LegendDefault_map = {
+	"name-pref-font": "pref-font-default",
+	"name-pref-theme": "pref-theme-default",
+	"name-pref-fontsize": "pref-fontsize-default",
+	"name-pref-fontsize-selectelm": "default"
+}
 function CookieChangeHook(name, val) {
-	var LegendDefault_map = {
-		"name-pref-font": "pref-font-default",
-		"name-pref-theme": "pref-theme-default"
-	}
 	CookieLevelHook(name, val)
 	if(LegendDefault_map[name] === val) {
 		cookie_json[name] = ""
@@ -27,13 +29,36 @@ function CookieChangeHook(name, val) {
 }
 /* PrefSetCb() */
 pref_input_el_arr = document.getElementsByTagName("input");
+pref_select_el_arr = document.getElementsByTagName("select");
+function InputSetEnable(value) {
+	var fontsize_select_el = document.getElementById("pref-fontsize-selectelm")
+	if(!fontsize_select_el) {
+		return
+	}
+	if (value === "pref-fontsize-default") {
+		fontsize_select_el.disabled = true;
+	} else {
+		fontsize_select_el.disabled = false;
+	}		
+}
+function SelectCheckSync() {
+
+}
 function input_changed_cb(ev) {
 	var val = ev.target.value
 	var name = ev.target.name
+	console.log(name, val)
+	if(name === "name-pref-fontsize") {
+		InputSetEnable(val)
+	}
 	CookieChangeHook(name, val)
 }
 for (var el_index = 0; el_index < pref_input_el_arr.length; el_index += 1) {
 	var el = pref_input_el_arr[el_index]
+	el.addEventListener('change', input_changed_cb)
+}
+for (var el_index = 0; el_index < pref_select_el_arr.length; el_index += 1) {
+	var el = pref_select_el_arr[el_index]
 	el.addEventListener('change', input_changed_cb)
 }
 function Json2Cookie() {
@@ -42,16 +67,22 @@ function Json2Cookie() {
 	}
 }
 /* PrefLegendCheckSync() */
-var LegendName_arr = ["name-pref-font", "name-pref-theme"]
-for (var name_index = 0; name_index < LegendName_arr.length; name_index += 1) {
-	var name = LegendName_arr[name_index]
-	var value = cookie_json[name]
-	console.log(name, value)
-	var input_el = document.getElementById(value)
-	console.log(input_el)
-	if (input_el) {
-		input_el.checked = true
+for (name in LegendDefault_map) {
+	var value = CookieGetKey(name)
+	if (value) {
+		var input_el = document.getElementById(value)
+		if (input_el) {
+			input_el.checked = true
+		}
 	}
+}
+/* InputCheckSync() */
+var pf = CookieGetKey("name-pref-fontsize")
+pf && InputSetEnable(pf)
+/* SelectCheckSync() */
+var fontsize_select_el = document.getElementById("pref-fontsize-selectelm")
+if (fontsize_select_el) {
+	fontsize_select_el.value = CookieGetKey("name-pref-fontsize-selectelm")
 }
 })()
 
