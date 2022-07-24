67c44554831f43ad4051575d5fc2ede9eb6fceab
willyang <22425315+willyywt@users.noreply.github.com>
2022-07-25 03:25:16 +0800

    [HandWritten] move font to separate file

--- _sass/make.sh
+++ _sass/make.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 sassc -t compressed common.scss > ../_includes/common.css
+sassc -t compressed font.scss > ../_includes/font.css
 sassc -t compressed light.scss > ../_includes/light.css
 sassc -t compressed dark.scss > ../_includes/dark.css
