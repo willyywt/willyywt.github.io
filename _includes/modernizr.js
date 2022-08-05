a2821df28b337fb993423ab2901e6d9123c3f3ec
willyang <22425315+willyywt@users.noreply.github.com>
2022-08-06 01:46:45 +0800

    [HandWritten] modernizr: es6/object

--- _includes/modernizr.js
+++ _includes/modernizr.js
@@ -1,6 +1,6 @@
 /*!
  * modernizr v3.12.0
- * Build https://modernizr.com/download?-classlist-localstorage-mediaqueries-urlsearchparams-addtest-dontmin
+ * Build https://modernizr.com/download?-classlist-es6object-json-localstorage-mediaqueries-urlsearchparams-addtest-dontmin
  *
  * Copyright (c)
  *  Faruk Ates
@@ -725,6 +725,49 @@
 !*/
 
   Modernizr.addTest('classlist', 'classList' in docElement);
+
+/*!
+{
+  "name": "ES6 Object",
+  "property": "es6object",
+  "notes": [{
+    "name": "unofficial ECMAScript 6 draft specification",
+    "href": "https://web.archive.org/web/20180825202128/https://tc39.github.io/ecma262/"
+  }],
+  "polyfills": ["es6shim"],
+  "authors": ["Ron Waldon (@jokeyrhyme)"],
+  "warnings": ["ECMAScript 6 is still a only a draft, so this detect may not match the final specification or implementations."],
+  "tags": ["es6"]
+}
+!*/
+/* DOC
+Check if browser implements ECMAScript 6 Object per specification.
+*/
+
+  Modernizr.addTest('es6object', !!(Object.assign &&
+    Object.is &&
+    Object.setPrototypeOf));
+
+/*!
+{
+  "name": "JSON",
+  "property": "json",
+  "caniuse": "json",
+  "notes": [{
+    "name": "MDN Docs",
+    "href": "https://developer.mozilla.org/en-US/docs/Glossary/JSON"
+  }],
+  "polyfills": ["json2"]
+}
+!*/
+/* DOC
+Detects native support for JSON handling functions.
+*/
+
+  // this will also succeed if you've loaded the JSON2.js polyfill ahead of time
+  //   ... but that should be obvious. :)
+
+  Modernizr.addTest('json', 'JSON' in window && 'parse' in JSON && 'stringify' in JSON);
 
 /*!
 {
