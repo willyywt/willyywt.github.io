d76502ab11ab8b5e80dbb56492a2a770246e8a15
willyang <22425315+willyywt@users.noreply.github.com>
2022-08-16 03:52:33 +0800

    [HandWritten] _include: remove inline css. Font css is still inlined

--- _sass/make.sh
+++ _sass/make.sh
@@ -1,6 +1,2 @@
 #!/bin/sh
-sassc -t compressed common.scss > ../_includes/common.css
-sassc -t compressed layout.scss > ../_includes/layout.css
 sassc -t compressed font.scss > ../_includes/font.css
-sassc -t compressed light.scss > ../_includes/light.css
-sassc -t compressed dark.scss > ../_includes/dark.css
