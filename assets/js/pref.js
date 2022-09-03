abe12409ba4c547bb1d2564431a72dbbc56c780f
willyang <22425315+willyywt@users.noreply.github.com>
2022-09-03 20:50:49 +0800

    [HandWritten] pref: move long line of customHeadHTML to pref.js

--- assets/js/pref.js
+++ assets/js/pref.js
@@ -1,9 +1,18 @@
 /* @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt Expat License */
 (function(){
-	window.addEventListener('DOMContentLoaded', l);
-	function l(){
-if (!Modernizr.localstorage) {
-	return
+window.addEventListener('DOMContentLoaded', l);
+function l() {
+var mel = document.querySelector("main")
+var mel_h1 = document.querySelector("main h1")
+if(!Modernizr.localstorage) {
+  var errlocalel = NoteEl("error", "Preferences are unavaliable because your browser does not support localStorage.")
+  mel.insertBefore(errlocalel, mel_h1.nextSibling)
+  return
+}
+if(!Modernizr.classlist) {
+  var mel_th = document.getElementById("theme")
+  var errclst = NoteEl("error", "Theme cannot be changed because your browser does not support classList.")
+  mel.insertBefore(errclst, mel_th.nextSibling)
 }
 function GCSReload() {
 	/* Try reloading giscus iframe. Due to XSS related restrictions we cannot modify anything in iframe. */
